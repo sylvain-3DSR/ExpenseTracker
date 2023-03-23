777f6adf6e18677ac6ed273bde83c27be2015406
sylvain-3DSR <58294074+sylvain-3DSR@users.noreply.github.com>
2023-03-23 10:39:30 +0100

    Cleaning initial project

--- app/src/main/java/com/sr/expensetracker/MainActivity.kt
+++ app/src/main/java/com/sr/expensetracker/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
+                    // change name of repository test
                 }
             }
         }
